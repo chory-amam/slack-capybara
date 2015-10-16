7eeece178bcf7125ff60ff41d65a8604a3f1168c
masahitojp <randomstep@gmail.com>
2015-10-16 18:13:16 +0900

    fixed #100 refactoring

--- src/main/java/handlers/DiagnosticsHandlers.java
+++ src/main/java/handlers/DiagnosticsHandlers.java
@@ -7,19 +7,19 @@ import java.util.Date;
 
 @SuppressWarnings("unused")
 public class DiagnosticsHandlers implements BotanMessageHandlers {
-    public static String PING_DESCRIPTION = "Reply with pong";
-    public static String TIME_DESCRIPTION = "Reply with current time";
+
     @Override
     public void register(final Robot robot) {
         robot.respond(
                 "(?i)ping\\z",
-                PING_DESCRIPTION,
-                message -> message.reply(message.getBody().substring(message.getBody().length() - 4).replace("i", "o").replace("I", "O"))
+                "Reply with pong",
+                message -> message.reply(message.getBody().substring(message.getBody().length() - 4)
+                        .replace("i", "o").replace("I", "O"))
         );
 
         robot.respond(
                 "(?i)time\\z",
-                TIME_DESCRIPTION,
+                "Reply with current time",
                 message -> message.reply(String.format("Server time is: %s", new Date()))
         );
     }
